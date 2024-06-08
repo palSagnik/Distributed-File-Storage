e83b05708fe422f8f9da8860643cf610f7873d9f
SAGNIK PAL <sagnik@SAGNIKs-MacBook-Air.local>
2024-06-08 22:19:04 +0530

    Feature: Write, Read, Delete of storage implemented

--- storage.go
+++ storage.go
@@ -1,12 +1,51 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
 
-type PathTransformFunc func(string) string
+func CASPathTransformFunc(key string) PathKey {
+	hash := sha1.Sum([]byte(key))
+	hashString := hex.EncodeToString(hash[:])
+
+	blockSize := 8
+	sliceLength := len(hashString) / blockSize
+
+	// Goes over the hashString and breaks it into a slice of substrings
+	// This is then used as path for the file
+	// blockSize -> size of each substring
+	// sliceLength -> number of substrings
+	paths := make([]string, sliceLength)
+	for i := 0; i < sliceLength; i++ {
+		from, to := i*blockSize, (i*blockSize)+blockSize
+		paths[i] = hashString[from:to]
+	}
+
+	return PathKey{
+		PathName: strings.Join(paths, "/"),
+		Filename: hashString,
+		Root: 	  paths[0],
+	}
+}
+
+type PathTransformFunc func(string) PathKey
+
+type PathKey struct {
+	PathName string
+	Filename string
+	Root     string
+}
+
+func (p PathKey) CompletePath() string {
+	return fmt.Sprintf("%s/%s", p.PathName, p.Filename)
+}
 
 var DefaultPathTransformFunc = func(key string) string {
 	return key
@@ -26,16 +65,69 @@ func NewStorage(config StorageConfig) *Storage {
 	}
 }
 
+func (s *Storage) Present(key string) bool {
+	pk := s.PathTransformation(key)
+	fullpath := pk.CompletePath()
+
+	_, err := os.Stat(fullpath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		} else {
+			fmt.Printf("Error in statting the file: %s", err)
+		}
+	}
+
+	return true
+}
+
+// This delete function for now is not taking in account
+// the probabibility of partial hash collision
+func (s *Storage) Delete(key string) error {
+	pk := s.PathTransformation(key)
+
+	defer func() {
+		log.Printf("deleted [%s] from disk", pk.Filename)
+	}()
+	if err := os.RemoveAll(pk.Root); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Storage) Read(key string) (io.Reader, error) {
+	f, err := s.readStream(key)
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	buffer := new(bytes.Buffer)
+	_, err = io.Copy(buffer, f)
+
+	return buffer, err
+}
+
+func (s *Storage) readStream(key string) (io.ReadCloser, error) {
+	pk := s.PathTransformation(key)
+
+	completePath := pk.CompletePath()
+	return os.Open(completePath)
+
+}
+
 func (s *Storage) writeStream(key string, r io.Reader) error {
 
-	pathName := s.PathTransformation(key)
+	pathKey := s.PathTransformation(key)
+	pathName := pathKey.PathName
+
 	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	filename := "somefilename"
-	completePath := pathName + "/" + filename
-
+	completePath := pathKey.CompletePath()
 	f, err := os.Create(completePath)
 	if err != nil {
 		return err
@@ -48,4 +140,5 @@ func (s *Storage) writeStream(key string, r io.Reader) error {
 
 	log.Printf("Written %d bytes to disk: %s", n, completePath)
 	return nil
+
 }
--- storage_test.go
+++ storage_test.go
@@ -2,20 +2,66 @@ package main
 
 import (
 	"bytes"
+	"fmt"
+	"io"
 	"testing"
 )
 
-func TestStorage(t *testing.T) {
+func TestStorageDeleteKey(t *testing.T) {
 	config := StorageConfig{
-		PathTransformation: DefaultPathTransformFunc,
+		PathTransformation: CASPathTransformFunc,
 	}
 
 	s := NewStorage(config)
-	key := "mygotesthere"
-	data := bytes.NewReader([]byte("this will work"))
+	key := "mygreatestgoal"
+	data := []byte("this will work")
 
-	if err := s.writeStream(key, data); err != nil {
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
 		t.Error(err)
 	}
 
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
+	}
+
+}
+func TestPathTransformFunc(t *testing.T) {
+	key := "mygreatestgoal"
+	pathkey := CASPathTransformFunc(key)
+	expectedFilenamekey := "bef06e07b129dc25191b1ccffffb0f6d05e64125"
+	expectedPathName := "bef06e07/b129dc25/191b1ccf/fffb0f6d/05e64125"
+
+	if pathkey.PathName != expectedPathName {
+		t.Errorf("wanted %s, got %s", expectedPathName, pathkey.PathName)
+	}
+	if pathkey.Filename != expectedFilenamekey {
+		t.Errorf("wanted %s, got %s", expectedFilenamekey, pathkey.Filename)
+	}
+	fmt.Println(pathkey.PathName)
+}
+
+func TestStorageWriteAndRead(t *testing.T) {
+	config := StorageConfig{
+		PathTransformation: CASPathTransformFunc,
+	}
+
+	s := NewStorage(config)
+	key := "mygreatestgoal"
+	data := []byte("this will work")
+
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	r, err := s.Read(key)
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := io.ReadAll(r)
+	if string(b) != string(data) {
+		t.Errorf("wanted %s, got %s", data, b)
+	}
+
+	fmt.Println(string(b))
 }
